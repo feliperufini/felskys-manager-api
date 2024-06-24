7b22b36ab98d1ff28a9b2dabf3fdbaefea31e6db
Felipe Andrade <37109398+feliperufini@users.noreply.github.com>
2024-06-24 11:20:45 -0400

    feat: adds zod validations to the env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { table } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -11,7 +12,7 @@ app.get('/hello', async () => {
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server running on http://localhost:3333')
